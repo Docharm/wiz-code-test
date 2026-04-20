2c85ce9974f16688f700aacd0c4ce13a0828c35e
이도연(Doyeon Lee) <dylee@cloocus.com>
2026-04-21 08:45:48 +0900

    clean init

---
+++ code-test/code-test.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_security_group" "test_sg" {
+  name        = "test-sg"
+  description = "test"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # 위험 설정
+  }
+}
